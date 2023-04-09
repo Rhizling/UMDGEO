822d97a5476ddd84e794146a3168586f522d4b8c
Aaron <achen4928@gmail.com>
2023-04-08 22:21:42 -0400

    Updated JS and HTML files

--- Geoguesser/Guessr.js
+++ Geoguesser/Guessr.js
@@ -1,41 +1,21 @@
+var previousMarker;
+var interval;
+
 function initMap() {
+  clearInterval(interval);
+
   var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 38.987003, lng: -76.942293 },
     zoom: 14,
     disableDefaultUI: true,
   });
 
-  // Create a custom zoom control and add it to the map
-  /*var zoomControlDiv = document.createElement("div");
-  var zoomControl = new CustomZoomControl(zoomControlDiv, map);*/
-
-  // Add the custom zoom control to the map
-  /*zoomControlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(zoomControlDiv);*/
-
   // Disable the zoom control and map type control
   map.setOptions({
     zoomControl: false,
     mapTypeControl: false,
+    clickableIcons: false,
   });
-
-  /*var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 37.7749, lng: -122.4194},
-      zoom: 8
-    });*/
-
-  // Add a marker to the map
-  var marker = new google.maps.Marker({
-    position: { lat: 38.987003, lng: -76.942293 },
-    map: map,
-    title: "College Park",
-  });
-
-  /*var marker = new google.maps.Marker({
-      position: {lat: 37.7749, lng: -122.4194},
-      map: map,
-      title: 'San Francisco'
-    });*/
 
   const result = getRandomLocation({ lat: 38.987003, lng: -76.942293 }, 0.785);
 
@@ -43,6 +23,44 @@ function initMap() {
     position: { lat: result.lat, lng: result.lng },
     pov: { heading: 165, pitch: 0 },
     zoom: 1,
+
+    linksControl: false,
+    panControl: false,
+    addressControl: false,
+    enableCloseButton: false,
+    zoomControl: false,
+    fullscreenControl: false,
+    enableCloseButton: false,
+    showRoadLabels: false,
+  });
+
+  var map;
+  var timerElement = document.getElementById("timer");
+  var counter = 10;
+
+  // Start the timer update interval
+  interval = setInterval(updateTimer, 1000);
+
+  function updateTimer() {
+    // Update the timer display
+    if (counter != -1) {
+      timerElement.innerHTML = counter;
+      counter--;
+    }
+  }
+
+  // Add a listener for the 'click' event
+  map.addListener("click", function (event) {
+    // Remove the previous marker if it exists
+    if (previousMarker) {
+      previousMarker.setMap(null);
+    }
+
+    // Create a marker at the clicked location and set it as the previous marker
+    previousMarker = new google.maps.Marker({
+      position: event.latLng,
+      map: map,
+    });
   });
 }
 
@@ -57,6 +75,13 @@ function getRandomLocation(center, radius) {
   // center is an object with properties lat and lng
   const lat = center.lat + ((Math.random() * 2 - 1) * radius) / 111;
   const lng = center.lng + ((Math.random() * 2 - 1) * radius) / (111 * Math.cos((center.lat * Math.PI) / 180));
+  return { lat, lng };
+}
+
+function getUserLocation() {
+  var position = previousMarker.getPosition();
+  lat = position.lat();
+  lng = position.lng();
   return { lat, lng };
 }
 
